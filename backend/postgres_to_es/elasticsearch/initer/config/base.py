1cdbf016b1d4dc8f9c2e9c49ec232b124a7a6427
xh4vm <h4vm@yandex.ru>
2022-07-27 01:25:37 +0500

    Инициализация индексов при запуске etl процесса

---
+++ backend/postgres_to_es/elasticsearch/initer/config/base.py
@@ -0,0 +1,22 @@
+from typing import Any
+import backoff
+from pydantic import BaseSettings, Field
+
+
+class ElasticsearchSettings(BaseSettings):
+    protocol: str = Field(..., env='ES_PROTOCOL')
+    user: str = Field(..., env='ES_USER')
+    password: str = Field(..., env='ES_PASSWORD')
+    host: str = Field(..., env='ES_HOST')
+    port: int = Field(..., env='ES_PORT')
+
+
+class ElasticsearchIndices(BaseSettings):
+    movies: str = Field(..., env='INDEX_MOVIES')
+    genres: str = Field(..., env='INDEX_GENRES')
+    persons: str = Field(..., env='INDEX_PERSONS')
+
+
+ELASTIC_CONFIG: ElasticsearchSettings = ElasticsearchSettings()
+ELASTIC_INDICES: ElasticsearchIndices = ElasticsearchIndices()
+BACKOFF_CONFIG: dict[str, Any] = {'wait_gen': backoff.expo, 'exception': Exception, 'max_value': 8}
---
+++ backend/postgres_to_es/elasticsearch/initer/elastic_initer.py
@@ -0,0 +1,58 @@
+import backoff
+from elasticsearch import Elasticsearch
+from typing import Any, Optional
+
+from config.base import BACKOFF_CONFIG, ElasticsearchSettings
+from config.logger import logger
+
+
+def es_conn_is_alive(es_conn: Elasticsearch) -> bool:
+    """Функция для проверки работоспособности Elasticsearch"""
+    try:
+        return es_conn.ping()
+    except Exception:
+        return False
+
+
+class ElasticIniter:
+
+    def __init__(
+        self, 
+        settings: ElasticsearchSettings, 
+        conn: Optional[Elasticsearch] = None
+    ) -> None:
+        self._conn: Elasticsearch = conn
+        self._settings: ElasticsearchSettings = settings
+
+    @property
+    def conn(self) -> Elasticsearch:
+        if self._conn is None or not es_conn_is_alive(self._conn):
+            self._conn = self._reconnection()
+
+        return self._conn
+
+    @backoff.on_exception(**BACKOFF_CONFIG, logger=logger)
+    def _reconnection(self) -> Elasticsearch:
+        logger.info('Reconnection elasticsearch...')
+        
+        if self._conn is not None:
+            logger.info('Closing already exists es connector...')
+            self._conn.close()
+
+        return Elasticsearch(
+                [
+                    (
+                        f'{self._settings.protocol}://{self._settings.user}:{self._settings.password}'
+                        f'@{self._settings.host}:{self._settings.port}'
+                    )
+                ]
+            )
+
+    @backoff.on_exception(**BACKOFF_CONFIG, logger=logger)
+    def create(self, index_name: str, mapping: dict[str, Any]):
+        if self.conn.indices.exists(index_name):
+            logger.info(f'[+] Index "{index_name}" already exists!')
+        else:
+            logger.info(f'[?] Creating index "{index_name}" with map...')
+            self.conn.indices.create(index=index_name, body=mapping)
+            logger.info(f'[+] Successfull creating index "{index_name}"!')
