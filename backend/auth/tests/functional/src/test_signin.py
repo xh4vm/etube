ecb9741cbc774e692a53b45b3dad13a67a18ac69
xh4vm <h4vm@yandex.ru>
2022-08-15 00:38:39 +0500

    Тест на ошибку авторизации

--- backend/auth/tests/functional/src/test_signin.py
+++ backend/auth/tests/functional/src/test_signin.py
@@ -7,14 +7,16 @@ pytestmark = pytest.mark.asyncio
 
 async def test_sign_in_login_error(make_post_request):
     # Попытка входа с ошибкой в логине.
-    response = await make_post_request(f'action/sign_in')
+    response = await make_post_request(f'auth/action/sign_in', data={'login': 'asd', 'password': 'asd'})
+
+    assert response.status == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
-async def test_sign_in_password_error(make_post_request):
-    # Попытка входа с ошибкой в пароле.
-    response = await make_post_request(f'action/sign_in')
+# async def test_sign_in_password_error(make_post_request):
+#     # Попытка входа с ошибкой в пароле.
+#     response = await make_post_request(f'action/sign_in')
 
 
-async def test_sign_in(make_post_request):
-    # Вход зарегистрированного пользователя.
-    response = await make_post_request(f'action/sign_in')
+# async def test_sign_in(make_post_request):
+#     # Вход зарегистрированного пользователя.
+#     response = await make_post_request(f'action/sign_in')
