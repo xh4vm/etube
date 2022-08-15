204bf914031eda0163c125094bb9f76dd2fa03a7
xh4vm <xoklhyip@yandex.ru>
2022-08-15 16:59:14 +0500

    Создание тестового токена

--- backend/auth/tests/functional/utils/auth/jwt.py
+++ backend/auth/tests/functional/utils/auth/jwt.py
@@ -11,15 +11,23 @@ from functional.conftest import HTTPResponse
 def get_jwt_claims(token: str) -> dict[str, Any]:
     return jwt.decode(token, key=CONFIG.API.JWT_SECRET_KEY, algorithms=CONFIG.API.JWT_DECODE_ALGORITHMS)
 
+
 def get_jwt_identity(token: str) -> int:
     return jwt.decode(token, key=CONFIG.API.JWT_SECRET_KEY, algorithms=CONFIG.API.JWT_DECODE_ALGORITHMS)['sub']
+
 
 def get_jwt_exp(token: str) -> int:
     return jwt.decode(token, key=CONFIG.API.JWT_SECRET_KEY, algorithms=CONFIG.API.JWT_DECODE_ALGORITHMS)['exp']  
 
+
 def get_jti(token: str) -> str:
     return jwt.decode(token, key=CONFIG.API.JWT_SECRET_KEY, algorithms=CONFIG.API.JWT_DECODE_ALGORITHMS)['jti']  
+
 
 def verify_exp_jwt(token: str) -> bool:
     exp = get_jwt_exp(token)
     return exp > datetime.now().timestamp()
+
+
+def create_token(claims: dict[str: Any]) -> str:
+    return jwt.encode(claims, key=CONFIG.API.JWT_SECRET_KEY, algorithm=CONFIG.API.JWT_ALGORITHM)
