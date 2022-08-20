aef751971bb2c042a9364d3be8510f57662689c6
xh4vm <h4vm@yandex.ru>
2022-08-20 15:21:09 +0500

    Gevent

--- backend/auth/restful/main.py
+++ backend/auth/restful/main.py
@@ -1,14 +1,6 @@
-# from gevent import monkey
-# monkey.patch_all()
-# from gevent.pywsgi import WSGIServer
+from gevent import monkey
+monkey.patch_all()
 
 from api.app import create_app
-from core.config import CONFIG
 
 app = create_app()
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=CONFIG.APP.AUTH_APP_PORT)
-    # http_server = WSGIServer(('0.0.0.0', CONFIG.APP.AUTH_APP_PORT), app)
-    # http_server.serve_forever()
