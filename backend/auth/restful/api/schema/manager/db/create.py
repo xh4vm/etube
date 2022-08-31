14c14550a561a87dd55a62c6d9d8d3d06761ad30
thunderlion <thunderlion@protonmail.com>
2022-08-31 12:56:50 +0300

    Уточнены заголовки

--- backend/auth/restful/api/schema/manager/db/create.py
+++ backend/auth/restful/api/schema/manager/db/create.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 
 
 class DbBodyParams(BaseModel):
-    target_date: date = Field(title='Today')
+    target_date: date = Field(title='Дата, для месяца которой нужно создать таблицу')
 
 class DbResponse(BaseModel):
-    message: str = Field(title='Message')
+    message: str = Field(title='Сообщение ответа')
